a78cc91286a95fc0c0bf02020c4ba20e19f17f0a
michoball <myunghun0114@gmail.com>
2022-09-11 01:45:51 +0900

    chore: delete withCredentials option in oauthLogin

--- frontend/src/features/user/user.api.js
+++ frontend/src/features/user/user.api.js
@@ -32,9 +32,7 @@ const login = async (userData) => {
 };
 
 const oauthLogin = async () => {
-  const res = await axios.get(API_URL + "/oauth/success", {
-    withCredentials: true,
-  });
+  const res = await axios.get(API_URL + "/oauth/success");
   if (res.status === 200) {
     localStorage.setItem("user", JSON.stringify(res.data));
   }
