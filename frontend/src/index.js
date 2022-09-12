2d42d7ca9705194c5954bc99c7913bc9caa342e0
michoball <myunghun0114@gmail.com>
2022-09-12 17:46:04 +0900

    chore: add Strict mode again

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,9 +10,11 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
