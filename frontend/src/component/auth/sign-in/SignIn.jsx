b4f30771b99519cd632512e30a8c533e7f9c6ad8
michoball <myunghun0114@gmail.com>
2022-09-26 01:22:03 +0900

    fix: fix oAuth bug

--- frontend/src/component/auth/sign-in/SignIn.jsx
+++ frontend/src/component/auth/sign-in/SignIn.jsx
@@ -73,8 +73,13 @@ const SignInForm = () => {
 
   const oAuthLoginHandler = async (service) => {
     let timer = null;
+    console.log(process.env.REACT_APP_MY_APP_URL);
+    console.log(process.env.REACT_APP_NODE_ENV);
 
-    const oAuthUrl = process.env.REACT_APP_MY_APP_URL;
+    const oAuthUrl =
+      process.env.REACT_APP_NODE_ENV === "production"
+        ? process.env.REACT_APP_MY_APP_URL
+        : "http://localhost:5000";
 
     const newWindow = window.open(
       `${oAuthUrl}/api/users/${service}`,
