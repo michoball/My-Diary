910b513ed95a802dec8a294dc451b9af881d4533
michoball <myunghun0114@gmail.com>
2023-05-29 22:05:17 +0900

    chore: app test

--- backend/server.js
+++ backend/server.js
@@ -46,12 +46,9 @@ app.use("/api/memos", require("./routes/memoRoutes"));
 app.use("/api/labels", require("./routes/labelRoutes"));
 app.use("/api/calendars", require("./routes/calendarRoutes"));
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to the diary app" });
-});
-const client_id = "O8Ug2dgQNcAFBIrdN9zZ";
-const client_secret = "14bFS5eGBn";
-const query = "번역할 문장을 입력하세요.";
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "Welcome to the diary app" });
+// });
 
 //Serve Frontend
 if (process.env.NODE_ENV === "production") {
@@ -65,12 +62,6 @@ if (process.env.NODE_ENV === "production") {
   app.get("/", (req, res) => {
     res.status(200).json({ message: "Welcome to the diary app" });
   });
-  // app.use(
-  //   cors({
-  //     origin: "http://localhost:3000",
-  //     credentials: true,
-  //   })
-  // );
 }
 
 app.use(errorHandler);
